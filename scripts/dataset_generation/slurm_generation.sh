ea7831fd607f08a1742f6525dd81187c4ac96fa7
MPascual <mario.pg02@gmail.com>
2025-05-30 13:15:38 +0200

    Update readme

--- scripts/dataset_generation/slurm_generation.sh
+++ scripts/dataset_generation/slurm_generation.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 #SBATCH --job-name=CoronaryDatasetGen
-#SBATCH --time=1-00:00:00    # Request 1 day of runtime, adjust as needed
-#SBATCH --mem=32G            # Request 32 GB of memory, adjust based on your dataset size
+#SBATCH --time=00:20:00    # Request 1 day of runtime, adjust as needed
+#SBATCH --constraint=cal
+#SBATCH --mem=4G            # Request 32 GB of memory, adjust based on your dataset size
 #SBATCH --ntasks=1           # Run a single task
-#SBATCH --cpus-per-task=4    # Request 4 CPUs for the task, adjust as needed
-#SBATCH --error=logs/dataset_gen.%J.err  # Standard error log file
-#SBATCH --output=logs/dataset_gen.%J.out # Standard output log file
+#SBATCH --cpus-per-task=1    # Request 4 CPUs for the task, adjust as needed
+#SBATCH --error=dataset_gen.%J.err  # Standard error log file
+#SBATCH --output=dataset_gen.%J.out # Standard output log file
 
 # Create logs directory if it doesn't exist
 mkdir -p logs
@@ -16,12 +17,12 @@ module load miniconda # Or anaconda, or specific python module
 
 # Activate your Conda environment
 # Replace 'ica_detection' with the actual name of your environment
-source activate ica_detection
+source activate ica
 
 # Define the path to your scripts and config file
-SCRIPT_DIR="/home/mpascual/research/projects/Coronary_Angiography_Detection/scripts"
+SCRIPT_DIR="/mnt/home/users/tic_163_uma/mpascual/fscratch/repos/Coronary_Angiography_Detection/scripts/dataset_generation"
 PYTHON_SCRIPT="${SCRIPT_DIR}/generate_dataset.py"
-CONFIG_FILE="${SCRIPT_DIR}/config.yaml"
+CONFIG_FILE="$./config.yaml"
 
 # Print some information to the log
 echo "Starting dataset generation script..."
